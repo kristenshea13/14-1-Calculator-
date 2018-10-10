dc9062ee6b5d54fb21712561193eaa8f8a3e8df3
Kristen Shea <kstar61@live.com>
2018-10-09 23:06:00 -0500

    updated comments

--- 14-1 (Calculator)/Program.cs
+++ 14-1 (Calculator)/Program.cs
@@ -6,7 +6,22 @@ namespace _14_1__Calculator_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //explain program, prompt user for equation, give example for correct format.
+            Console.WriteLine("Calculator to add, subtract, multiply, or divide two numbers. ");
+            Console.Write("Please type out equation to be calculated. (Example: 1 + 1) ");
+
+            //put equation into string
+            string equation = Console.ReadLine();
+
+            //create new instance of calculator class using equation string as input.
+            Calculator calculator = new Calculator(equation);
+
+            //initiate method from calculator class to calculate equation
+            calculator.Calculate();
+
+
+            Console.ReadLine();
+
         }
     }
 }
